72cce3988af58f78ea115cbc06f6f4e1ba2173b0
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2023-05-12 14:08:53 +0000

    Release 0.0.24

--- src/api/resources/languages/client/Client.ts
+++ src/api/resources/languages/client/Client.ts
@@ -32,7 +32,7 @@ export class Languages {
                 Authorization: await this._getAuthorizationHeader(),
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@squidex/squidex",
-                "X-Fern-SDK-Version": "0.0.23",
+                "X-Fern-SDK-Version": "0.0.24",
             },
             contentType: "application/json",
             timeoutMs: 60000,
